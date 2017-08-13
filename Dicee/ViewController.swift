58d8283f63b0280f22a2f9620dede579576b6e71
Amit Biswas <biswas1992@hotmail.com>
2017-08-13 03:34:50 -0400

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Amit Biswas on 8/13/17.
+//  Copyright © 2017 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var randomDiceeIndex1 : Int = 0
+    var randomDiceeIndex2 : Int = 0
+    
+    @IBOutlet weak var diceeImageView1:
+    UIImageView!
+   
+    
+    @IBOutlet weak var diceeImageView2: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPreesed(_ sender: UIButton) {
+    
+    randomDiceeIndex1 = Int(arc4random_uniform(6))
+    randomDiceeIndex2 = Int(arc4random_uniform(6))
+        
+    }
+
+}
+
